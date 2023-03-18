6e8c149cac7b39254c15d9ab3323f07ad4f4e95e
wurui <1341531859@qq.com>
2023-03-18 18:29:43 +0800

    note setx for VCPKG_ROOT

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 sudo ln -s $(xcrun --show-sdk-path)/System/Library/Frameworks/GLUT.framework/Headers $(xcrun --show-sdk-path)/usr/include/GL
 xcrun clang -framework GLUT -framework OpenGL -DGL_SILENCE_DEPRECATION $1
 # set VCPKG_ROOT=%USERPROFILE%\Documents\vcpkg
+# setx VCPKG_ROOT %USERPROFILE%\Documents\vcpkg
+# RefreshEnv -> choco
