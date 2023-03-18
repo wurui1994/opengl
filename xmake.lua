32ddfb1ebf5b68821c6bf368410e424d09ab6460
wurui1994 <1341531859@qq.com>
2023-03-18 14:49:33 +0800

    force win32 gui

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,10 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c99", "c++11")
 
+if is_plat("windows") then 
+    add_ldflags("-subsystem:windows", "-entry:mainCRTStartup", {force=true}) 
+end
+
 if is_plat("macosx") then
     add_frameworks("GLUT", "OpenGL")
     add_defines("GL_SILENCE_DEPRECATION")
